b80c09576d0d4a363812c293e8b802f38ff56881
Pramod R <pramod19workspace@gmail.com>
2022-10-19 19:14:48 +0530

    validationForm

--- Day 25/validation.js
+++ Day 25/validation.js
@@ -1,61 +1,69 @@
-function validation(){
-    var name=document.getElementById("name").value;
-    var email=document.getElementById("email").value;
-    var phone=document.getElementById("phone").value;
+function validation()
+            {
 
-    if(name==""){
-        document.getElementById("err").innerHTML="Please Enter Name";
-        document.getElementById("name").focus();
-        return false;
-    }
+                var name=document.getElementById("name").value;
+                var email=document.getElementById("email").value;
+                var phone=document.getElementById("phone").value;
 
-    if(email==""){
-        document.getElementById("err").innerHTML="Enter Email ID"
-        document.getElementById("email").focus();
-        return false;
-    }
-    else{
-        var exp_email1=/^[a-zA-Z0-9\-\_\.]+\@[a-zA-Z0-9\-\_\.]+\.[a-z]{2,4}$/;
-        if(!exp_email1.test(email)){
-            document.getElementById('err').innerHTML="Enter valid Email ID";
-            document.getElementById("email").focus();
-            return false;
-        }
-        else{
-            document.getElementById('err').innerHTML="";
-        }
+                if(name==""){
+                    document.getElementById("errorMsg").innerHTML="Enter Name";
+                    document.getElementById("name").focus();
+                    return false;
+                }
+                else{
+                    var reg_name=/^[a-zA-z]{3}/;
+                    if(!reg_name.test(name)){
+                        document.getElementById("errorMsg").innerHTML="Enter Proper name";
+                        document.getElementById("name").focus();
+                        return false;
+                    }
+                    // else{
+                    //     document.getElementById("errorMsg").innerHTML="";
+                    // }
+                }
 
-    if(phone==""){
-        document.getElementById("err").innerHTML="Please Enter Name";
-        document.getElementById("name").focus();
-        return false;
-    }
-    else{
-        var exp_phone=/^[0-9]{10}$/;
-        if(!exp_phone.test(phone)){
-            document.getElementById('err').innerHTML="Enter valid Phone Number";
-            document.getElementById("phone").focus();
-            return false;
+                if(email==""){
+                    document.getElementById("errorMsg").innerHTML="Enter Email";
+                    document.getElementById("email").focus();
+                    return false;
+                }
+                else{
+                    var reg_email=/^[a-zA-Z0-9\.\_\@\-]+@[a-zA-z]+\.[a-z]{2,4}/;
+                    if(!reg_email.test(email)){
+                        document.getElementById("errorMsg").innerHTML="Enter Proper email ID";
+                        document.getElementById("email").focus();
+                        return false;
+                    }
+                }
 
-        }
-        else{
-            document.getElementById('err').innerHTML="";
+                if(phone==""){
+                    document.getElementById("errorMsg").innerHTML="Enter Phone Number";
+                    document.getElementById("phone").focus();
+                    return false;
+                }
+                else{
+                    var reg_phone=/^[0-9]{10}$/;
+                    if(!reg_phone.test(phone)){
+                        document.getElementById("errorMsg").innerHTML="Enter Proper Phone Number";
+                        document.getElementById("phone").focus();
+                        return false;
+                    }
 
-        }
-    }
-    }
+                
 
-    return true;
-}
 
-function sameAddress(){
-    var option=document.getElementById("same");
+                return true;
 
-    if(option.checked==true){
-        document.getElementById("permanentAddress").value=document.getElementById("currentAddress").value;
-    }
-    if(option.checked==false){
-        document.getElementById("permanentAddress").value="";
+            }
+            }
 
-    }
-}
+            function sameAddress(){
+                var option=document.getElementById("same");
+                if(option.checked==true){
+                    document.getElementById("permanentAddress").value=document.getElementById("currentAddress").value;
+                }
+                if(option.checked==false){
+                    document.getElementById("permanentAddress").value="";
+
+                }
+            }
